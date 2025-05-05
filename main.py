47a4cb1af527e7451528051be222e31e60abb645
prakhar-kt <rahkarp@gmail.com>
2025-05-05 17:46:40 +0530

    add players

--- main.py
+++ main.py
@@ -1,19 +1,34 @@
 import pygame
 from constants import *
+from player import Player
+
 
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    x = SCREEN_WIDTH/2
+    y = SCREEN_HEIGHT/2
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(x, y)
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        updatable.update(dt)
         screen.fill("Black")
+        for item in drawable:
+            item.draw(screen)
+
         pygame.display.flip()
 
-
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
